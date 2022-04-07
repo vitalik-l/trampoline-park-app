eb2aa87005fc7db07367e55102f9890660fd370d
Vitaly <mirenrich@yandex.ru>
2022-04-07 16:06:02 +0300

    initial

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
 import Button from '@mui/material/Button';
+import { Client } from './Client';
 
 export const App = () => {
-    return <Button variant="contained">Hello World</Button>
-}
+  return (
+    <div>
+      <Button variant="contained">Button</Button>
+      <Client />
+    </div>
+  );
+};
---
+++ src/components/Client.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+export const Client = () => {
+  return (
+    <div
+      css={css`
+        width: 10rem;
+        height: 10rem;
+        background: azure;
+      `}
+    >
+      test
+    </div>
+  );
+};
