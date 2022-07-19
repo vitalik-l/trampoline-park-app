7e29e710234a8b82450ebb291258c0d81e5530c1
Vitaly <mirenrich@yandex.ru>
2022-07-19 16:17:46 +0300

    added electron

---
+++ electron/index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const indexHtmlPath = path.resolve('./dist/index.html');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile(indexHtmlPath);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
